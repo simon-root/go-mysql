cfbd1e6686ec0334d3e0d803e7b8736517c0ee0f
xiaobiao <xiaobiao@kuaishou.com>
2021-12-03 17:34:55 +0800

    只大小适中的缓存块

--- utils/bytes_buffer_pool.go
+++ utils/bytes_buffer_pool.go
@@ -31,7 +31,7 @@ func BytesBufferGet() (data *bytes.Buffer) {
 }
 
 func BytesBufferPut(data *bytes.Buffer) {
-	if data == nil || len(data.Bytes()) < 4*1024 || len(data.Bytes()) > TooBigBlockSize {
+	if data == nil || len(data.Bytes()) > TooBigBlockSize {
 		return
 	}
 
